d070fef3347559546c2ffdd289f6e071f96526b5
HarryMinasyan <eachbase@gmail.com>
2021-04-20 16:09:27 -0700

    CI/CD fix

--- client/pages/profile.js
+++ client/pages/profile.js
@@ -1,20 +1,17 @@
-import Head from "next/head"
+import Head from "next/head";
 import {  ProfilePage } from "@eachbase/pages"
 
-export default function Profile (props) {
-
-  return (
-    <>
-      <Head>
-        {props.meta || <title>welcome menuz</title>}
-      </Head>
-      <ProfilePage {...props.pageData}/>
-
-    </>
-  )
+export default function Profile(props) {
+    return (
+        <>
+            <Head>{props.meta || <title>welcome menuz</title>}</Head>
+            <ProfilePage {...props.pageData}/>
+            <div>Profile Page</div>
+        </>
+    );
 }
 
 Profile.getInitialProps = async () => {
-  let res = {meta: `<title>inchvor ban</title>`, pageData: null}
-  return {...res}
-}
+    let res = { meta: `<title>inchvor ban</title>`, pageData: null };
+    return { ...res };
+};
