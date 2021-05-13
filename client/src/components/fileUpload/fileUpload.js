51acf0592e99b0d73f03fe16b8904d38f3dc2d99
hovhannesa <hovhannesa@eachbase.com>
2021-05-13 16:26:36 +0400

    created FileUpload dropzone component

---
+++ client/src/components/fileUpload/fileUpload.js
@@ -0,0 +1,61 @@
+import { Container } from "./style";
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import { v4 as uuid } from "uuid";
+import { TiDelete } from "react-icons/ti";
+
+export const FileUpload = ({
+  title = "File",
+  maxSize = 2048,
+  files = [],
+  onChange = () => {},
+}) => {
+  const onDrop = useCallback((acceptedFiles) => {
+    onChange(
+      acceptedFiles.map((file) =>
+        Object.assign(file, {
+          preview: URL.createObjectURL(file),
+          id: uuid(),
+        })
+      )
+    );
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    accept: "image/*",
+    onDrop,
+  });
+
+  console.log(files);
+  return (
+    <Container {...getRootProps()} isDragActive={isDragActive}>
+      <div className="uploaded-files">
+        {files.map((file) => (
+          <div
+            key={file.id}
+            className="file-preview"
+            style={{ backgroundImage: `url(${file.preview})` }}
+          >
+            <div
+              className="remove"
+              onClick={(e) => {
+                e.stopPropagation();
+                onChange(files.filter((cFile) => cFile.id !== file.id));
+              }}
+            >
+              <TiDelete />
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="content-container">
+        <div className="title">
+          Drag & Drop or <span className="active">Upload</span> {title}
+        </div>
+        <div className="acceptable-file-size-noth">
+          Max size {Math.round(maxSize / 1024)}mb
+        </div>
+      </div>
+      <input {...getInputProps()} />
+    </Container>
+  );
+};
