e4080ce1e7b1074f8337bf6db6e643a9f249de09
HarryMinasyan <>
2021-05-26 22:21:13 -0700

    backend refactor

--- api/src/owner/owner.module.ts
+++ api/src/owner/owner.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from 'src/auth';
-import { OwnerController } from './owner.controller';
-import { OwnerService } from './owner.service';
+import { Module } from "@nestjs/common";
+import { AuthModule } from "src/auth";
+import { OwnerSanitizer } from "./interceptor";
+import { OwnerController } from "./owner.controller";
+import { OwnerService } from "./owner.service";
 
 @Module({
   imports: [AuthModule],
   controllers: [OwnerController],
-  providers: [OwnerService],
+  providers: [OwnerService, OwnerSanitizer],
 })
 export class OwnerModule {}
