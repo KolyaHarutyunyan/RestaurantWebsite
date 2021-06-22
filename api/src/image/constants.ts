87f045367d9f0d492ea1bfd7f2a6c275a7bd7d0b
HarryMinasyan <>
2021-06-22 12:21:50 -0700

    change bucket name for master

--- api/src/image/constants.ts
+++ api/src/image/constants.ts
@@ -1,7 +1,7 @@
 export const AWS_ACCESS_KEY_ID = 'AKIAWFRERSLYBMQGIRHE';
 export const AWS_ACCESS_SECRET_KEY = '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';
 export const AWS_REGION = 'us-west-1';
-export const IMAGE_BUCKET = 'menuz-images';
+export const IMAGE_BUCKET = 'menumango';
 
 export const summaries = {
   ADD_IMAGE: `Add an image in the system. Stores the image in the cloud and returns the id of an image    ------DONE`,
