f8f4ab1bb0bf2a74d76afbaa78bdc8915b88af2b
HarryMinasyan <>
2021-07-14 11:53:31 -0700

    changed config

--- serverConfig/scripts/cleanup.sh
+++ serverConfig/scripts/cleanup.sh
@@ -3,5 +3,5 @@
 #remove everything from admin and client
 # rm -R -f /var/www/menuzMini/admin/*
 # rm -R -f /var/www/menuzMini/webapp/*
-rm -R -f /var/www/menuz/api/*
+rm -R -f /var/www/menuz/*
 rm -R -f /etc/nginx/sites-enabled/menuz.nginx.config
