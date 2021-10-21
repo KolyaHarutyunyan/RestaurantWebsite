5acb427114f513653f3309ce5c95558c3bea4cfe
Davo959 <davo.petrosyan959@gmail.com>
2021-10-21 12:08:11 +0400

    add changes

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,9 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 module.exports = withBundleAnalyzer({
+
+  exportTrailingSlash: true,
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
