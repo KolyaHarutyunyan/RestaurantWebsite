5a22f6d73c4806e93c565655ae1c7e182304b6ba
HarryMinasyan <>
2021-10-21 23:29:19 -0700

    merged with Dev' g

--- api/src/constants.ts
+++ api/src/constants.ts
@@ -3,7 +3,7 @@ export const MONGO_DUPLICATE_KEY = 11000;
 export const COMPANY_EMAIL = 'eachbase@gmail.com';
 export const PORT = 8005;
 
-const mode = ['local', 'development', 'production'][1];
+const mode = ['local', 'development', 'production'][2];
 export const BASE_URL = {
   local: 'http://localhost:8005/api',
   development: 'https://menuz.eachbase.com/api',
