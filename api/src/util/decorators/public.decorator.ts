3a63462d6018a857ea0b149e04454a5a85496871
HarryMinasyan <>
2022-02-11 14:15:30 -0800

    api change

--- api/src/util/decorators/public.decorator.ts
+++ api/src/util/decorators/public.decorator.ts
@@ -1,2 +1,8 @@
 import { SetMetadata } from '@nestjs/common';
 export const Public = () => SetMetadata('isPublic', true);
+export const VerifyCall = () => {
+  console.log('Verified ');
+  return (target, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) => {
+    console.log('decoreatoandowd');
+  };
+};
