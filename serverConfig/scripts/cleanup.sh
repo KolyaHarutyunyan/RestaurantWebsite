a8f432a85635df48ea2667ee171e1de5886c8fbf
HarryMinasyan <>
2022-02-15 11:03:29 -0800

    deployment cleanup

--- serverConfig/scripts/cleanup.sh
+++ serverConfig/scripts/cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #remove everything from admin and client
-# rm -R -f /var/www/menuzMini/admin/*
-# rm -R -f /var/www/menuzMini/webapp/*
+rm -R -f /var/www/menumango/admin/*
+rm -R -f /var/www/menumango/webapp/*
 rm -R -f /var/www/menumango/api/*
 rm -R -f /etc/nginx/sites-enabled/menumango.live.nginx.config
