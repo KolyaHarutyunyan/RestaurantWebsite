c464b68c2756b9a27c097b1cd2ffaaa13dc333ce
HarryMinasyan <>
2022-05-31 17:24:53 -0700

    updated nginx deploy config

--- serverConfig/scripts/restart_server.sh
+++ serverConfig/scripts/restart_server.sh
@@ -5,11 +5,11 @@ sudo service nginx restart
 # service nginx status
 
 # updating the .env file
-cd /var/www/menumango/api
-npm install
+# cd /var/www/menumango/api
+# npm install
 # sudo cp ../config/.env ./constants/
 
 # node restartsudo 
-cd /var/www/menumango/api
+# cd /var/www/menumango/api
 #sudo pm2 start /var/www/menumango/api/main.js --name "menumango"
 sudo pm2 restart menumango
